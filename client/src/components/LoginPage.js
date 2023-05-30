3957e31ab1b2a7f3eb6f0f742bd266c30de13c06
Nick Gastis <nickgastis1@gmail.com>
2023-05-30 11:01:57 -0400

    deploy test2

---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoginButton from './LoginButton'
+import "./styles/LoginPage.css"
+
+function LoginPage() {
+    return (
+        <div className='loginPage-cont' >
+            <h1>Atlas</h1>
+            <LoginButton />
+
+        </div>
+
+    )
+}
+
+export default LoginPage
